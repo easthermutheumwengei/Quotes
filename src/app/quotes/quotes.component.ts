84d756e65be04ecda718a80ca584b0a91d9e6c12
easthermutheumwengei <easther.mwengei@student.moringaschool.com>
2021-07-19 06:58:53 +0300

    add new dates to the quotes

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,6 +1,6 @@
 import { Quote } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
-import { Quote } from '../quotes';
+
 
 @Component({
   selector: 'app-quotes',
@@ -11,7 +11,12 @@ export class QuotesComponent implements OnInit {
 
   title: 'Quotes';
   quotes: Quote[] = [
-    new Quote(1,'Henry David Thoreau','Go confidently in the direction of your dreams! Live the life you have imagined')
+    new Quote(1, 'Henry David Thoreau', 'Go confidently in the direction of your dreams! Live the life you have imagined', 'Esther', new Date(2021, 7, 22), 67, 5),
+    new Quote(2, 'Mae West', 'You only live once, but if you do it right, once is enough', 'Marion', new Date(2021, 7, 22), 67, 5),
+    new Quote(1, 'Henry David Thoreau', 'Go confidently in the direction of your dreams! Live the life you have imagined', 'Esther', new Date(2021, 7, 22), 67, 5),
+    new Quote(1, 'Henry David Thoreau', 'Go confidently in the direction of your dreams! Live the life you have imagined', 'Esther', new Date(2021, 7, 22), 67, 5),
+    new Quote(1, 'Henry David Thoreau', 'Go confidently in the direction of your dreams! Live the life you have imagined', 'Esther', new Date(2021, 7, 22), 67, 5),
+    new Quote(1,'Henry David Thoreau','Go confidently in the direction of your dreams! Live the life you have imagined','Esther', new Date(2021,7,22),67,5),
   ]
 
 
