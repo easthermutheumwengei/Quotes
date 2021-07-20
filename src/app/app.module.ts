50d8a0c18c35535e11c6747d586e04df9308fe13
easthermutheumwengei <easther.mwengei@student.moringaschool.com>
2021-07-20 08:39:45 +0300

    add a form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { QuotesComponent } from './quotes/quotes.component';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 import { HighlightDirective } from './highlight.directive';
 import { PostedDatePipe } from './posted-date.pipe';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { PostedDatePipe } from './posted-date.pipe';
     QuotesComponent,
     QuoteDetailComponent,
     HighlightDirective,
-    PostedDatePipe
+    PostedDatePipe,
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule,
